368dd1be89749087dcc05c1a5089e561cec27770
hasan.tabanli <hasan.tabanli@06GZLSB-0010>
2022-09-01 17:07:24 +0300

    Object Sample

--- CSharpFeatures/Object.cs
+++ CSharpFeatures/Object.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace CSharpFeatures
 {
-    internal class Object
+    public class Human
     {
+        public int Age;
+        public string Name;
+
+        public void Eat()
+        {
+            Console.WriteLine(Name + "(" + Age + ")" + " eating");
+        }        
+        public void Sleep()
+        {
+            Console.WriteLine(Name + "(" + Age + ")" + " sleeping");
+        }
     }
 }
--- CSharpFeatures/Program.cs
+++ CSharpFeatures/Program.cs
@@ -7,6 +7,21 @@ namespace CSharpFeatures
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            #region Object Sample 
+            Human human = new Human();  //An object is an instance of a class. A class can have as many instances as needed. 
+            human.Age = 18;             //For example Hasan and John is a instance of Human.
+            human.Name = "Hasan";
+
+            Human human2 = new Human();
+            human2.Age = 17;
+            human2.Name = "John";
+
+            human.Eat();
+            human.Sleep();
+            human2.Eat();
+            human2.Sleep();
+            #endregion
         }
     }
 }
