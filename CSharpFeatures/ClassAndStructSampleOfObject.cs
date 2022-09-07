172672ac8f9993babdbad77d3b4ab0bcbef6783c
hasan.tabanli <hasan.tabanli@06GZLSB-0010>
2022-09-07 16:48:24 +0300

    Struct Objects vs Class Objects

---
+++ CSharpFeatures/ClassAndStructSampleOfObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFeatures
+{
+    public class HumanClass
+    {
+        public int Age;
+        public string Name;
+
+        public HumanClass(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine(Name + "(" + Age + ")" + " eating");
+        }        
+        public void Sleep()
+        {
+            Console.WriteLine(Name + "(" + Age + ")" + " sleeping");
+        }
+    }
+
+    public struct HumanStruct
+    {
+        public int Age;
+        public string Name;
+
+        public HumanStruct(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine(Name + "(" + Age + ")" + " eating");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine(Name + "(" + Age + ")" + " sleeping");
+        }
+    }
+}
--- CSharpFeatures/Program.cs
+++ CSharpFeatures/Program.cs
@@ -6,21 +6,51 @@ namespace CSharpFeatures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! \n\n");
 
             #region Object Sample 
-            Human human = new Human();  //An object is an instance of a class. A class can have as many instances as needed. 
-            human.Age = 18;             //For example Hasan and John is a instance of Human.
-            human.Name = "Hasan";
 
-            Human human2 = new Human();
-            human2.Age = 17;
-            human2.Name = "John";
+            #region For Class
+            HumanClass human = new HumanClass(18, "Hasan");   //An object is an instance of a class. A class can have as many instances as needed. 
+                                                              //For example Hasan and John is a instance of Human.
 
             human.Eat();
-            human.Sleep();
+
+            HumanClass human2 = human;
+
+            human2.Age = 17;                                  // Classes are reference types. So a variable of a class object holds a reference to the address of the object on the managed heap
+            human2.Name = "John";                             // So output will Hasan(18) eating
+                                                              //                John(17) eating
+                                                              //                John(17) eating
+
+            human.Eat();
             human2.Eat();
-            human2.Sleep();
+
+            Console.WriteLine("\nClass Object Sample Result\n");
+            #endregion
+
+            #region For Struct
+
+            HumanStruct humanStruct = new HumanStruct(18, "Hasan");   //An object is an instance of a class. A class can have as many instances as needed. 
+                                                                      //For example Hasan and John is a instance of Human.
+
+            humanStruct.Eat();
+
+            HumanStruct humanStruct2 = humanStruct;
+
+            humanStruct2.Age = 17;                                  // Structs are value types. So a variable of a struct object holds a copy of the entire object
+            humanStruct2.Name = "John";                             // So output will Hasan(18) eating
+                                                                    //                Hasan(18) eating
+                                                                    //                John(17) eating
+
+            humanStruct.Eat();
+            humanStruct2.Eat();
+
+            Console.WriteLine("\nStruct Object Sample Result\n");
+            #endregion
+
+            Console.WriteLine("\n\nEnd Of Object Sample");
+
             #endregion
         }
     }
