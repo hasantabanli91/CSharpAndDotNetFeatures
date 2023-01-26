fed894cc7f38605b8e697b5204645c35d93ec008
hasan.tabanli <hasan.tabanli@06GZLSB-0010>
2023-01-26 10:29:53 +0300

    abstract and interface sample

---
+++ CSharpFeatures/AbstractAndInterface.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFeatures
+{
+    public class Mercedes : Cars, ILuxFeatures
+    {
+        public override string Color { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string Model { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void GetWheeleCount()
+        {
+            Console.WriteLine("4");
+        }
+
+        public void SeatHeating()
+        {
+            Console.WriteLine("Seats can be heat");
+        }
+    }
+
+    public abstract class Cars : IWheeleCount // Abstract class defines features what a class have to do.
+    {
+        public abstract string Color { get; set; }
+        public abstract string Model { get; set; }
+        public void Drive()
+        {
+            Console.WriteLine("Driving");
+        }
+        public abstract void GetWheeleCount();
+    }
+
+    public interface IWheeleCount // Interfaces defines features what a class need.
+    {
+        void GetWheeleCount();
+    }
+
+    public interface ILuxFeatures
+    {
+        void SeatHeating();
+    }
+}
