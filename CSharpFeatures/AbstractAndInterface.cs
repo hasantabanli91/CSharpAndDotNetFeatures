e1a4565a0560c24124ad7b6e27b23c96ded1899a
hasan.tabanli <hasan.tabanli@06GZLSB-0010>
2023-01-26 11:06:47 +0300

    fix

--- CSharpFeatures/AbstractAndInterface.cs
+++ CSharpFeatures/AbstractAndInterface.cs
@@ -20,20 +20,20 @@ namespace CSharpFeatures
         }
     }
 
-    public abstract class Cars : IWheeleCount // Abstract class defines features what a class have to do.
-    {
-        public abstract string Color { get; set; }
-        public abstract string Model { get; set; }
-        public void Drive()
+    public abstract class Cars : IWheeleCount           // Abstract class defines features what a class can need.
+    {                                                   // A class can extend only one abstract class
+        public abstract string Color { get; set; }      // Abstract properties and Normal properties can be define in Abstract classes 
+        public abstract string Model { get; set; }      // Abstract classes can implements other abstract classes and interfaces too.
+        public void Drive()                             // Abstract classes can take access modifiers.
         {
-            Console.WriteLine("Driving");
-        }
+            Console.WriteLine("Driving");               
+        }                                               
         public abstract void GetWheeleCount();
     }
 
-    public interface IWheeleCount // Interfaces defines features what a class need.
-    {
-        void GetWheeleCount();
+    public interface IWheeleCount                        // Interfaces defines features what a class have to have.
+    {                                                    // Interfaces supports multiple inheritence
+        void GetWheeleCount();                           // Everythings in interfaces are public
     }
 
     public interface ILuxFeatures
